17e6eee34c7fefdc6ee3d8638f538fec452bb756
Hua Thanh Tam <huatam391@gmail.com>
2019-04-12 09:08:33 +0700

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
       root "static_pages#home"
 
+      get "sessions/new"
+      get "/login", to: "sessions#new"
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
       get "/signup", to: "users#new"
       resources :users
   end
