ece9aa6a946fff7daa860dabb497ee0ff620d077
Hua Thanh Tam <huatam391@gmail.com>
2019-04-12 09:08:33 +0700

    add sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def full_title page_title
-    # base_title = I18n.t "helper.application.base_title"
     base_title = t ".fastfood"
     if page_title.blank?
       base_title
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :suggestions, dependent: :destroy
   enum role: {normal: 0, admin: 1}
-  validates :name,  presence: true, length: {maximum: Settings.name.maximum}
+  before_save :downcase_email
+  validates :name, presence: true,
+    length: {maximum: Settings.users.name.maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.email.maximum},
+  validates :email, presence: true,
+    length: {maximum: Settings.users.email.maximum},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.users.password.minimum}
+
+  def downcase_email
+    email.downcase!
+  end
 end
